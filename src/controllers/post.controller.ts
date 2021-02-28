b4a7268ddd73eb19c898790c9c51eaa9e44e4f84
DaniPinelli <elidaniypri@gmail.com>
2021-02-28 16:08:09 -0300

    CRUD completo y convertido a Js

--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -18,3 +18,29 @@ export async function createPost(req: Request, res: Response) {
     });
     
 }
+
+export async function getPost(req: Request, res: Response): Promise<Response> {
+   const id = req.params.id;
+   const conn = await connect();
+   const posts = await conn.query('SELECT * FROM post WHERE id = ?', [id]);
+   return res.json(posts[0]);
+}
+
+export async function deletePost(req: Request, res: Response){
+    const id = req.params.id;
+    const conn = await connect();
+    await conn.query('DELETE FROM post WHERE id = ?', [id]);
+    return res.json({
+        message: 'Post Deleted'
+    });
+ }
+
+ export async function updatePost(req: Request, res: Response){
+    const id = req.params.id;
+    const updatePost: Post = req.body;
+    const conn = await connect();
+    await conn.query('UPDATE post set ? WHERE id = ?', [updatePost, id]);
+    return res.json({
+        message: 'Post Updated'
+    });
+ }
